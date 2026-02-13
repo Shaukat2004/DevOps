e7db779bd9fb2b60b47c2713b86c4955a2d689a8
Shantanu <shantanukundu2001@gmail.com>
2026-02-13 20:44:35 +0530

    shan-kun branch Initial Commit

--- userincome/models.py
+++ userincome/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 
-#Model Creation goes here
+#Model Creation goes inside this portion
 
 
 class UserIncome(models.Model):
